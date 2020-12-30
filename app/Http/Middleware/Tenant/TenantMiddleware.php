9d7a296916ea7d667950d4dd32d83cfcfd54db44
Renan Joppert <renanjoppert@outlook.com>
2020-12-30 20:00:30 -0300

    Identificação do domínio, busca dos dados no DB e 404 em caso de falha.

--- app/Http/Middleware/Tenant/TenantMiddleware.php
+++ app/Http/Middleware/Tenant/TenantMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware\Tenant;
 
+use App\Models\Company;
 use Closure;
 
 class TenantMiddleware
@@ -9,14 +10,26 @@ class TenantMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        dd('Aqui');
+
+        if( !$company = $this->getCompany($request->getHost()) ){
+            echo 'Não foi possível acessar a aplicação para este domínio';
+            http_response_code(404);
+            die();
+        }
+
+
 
         return $next($request);
+    }
+
+    public function getCompany($host)
+    {
+        return Company::where('domain', $host)->first();
     }
 }
