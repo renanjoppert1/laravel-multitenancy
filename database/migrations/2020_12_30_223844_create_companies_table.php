3c16bf074ac6d5b2293190379fe2c8535a057012
Renan Joppert <renanjoppert@outlook.com>
2020-12-30 19:42:59 -0300

    Modelagem da tabela company

--- database/migrations/2020_12_30_223844_create_companies_table.php
+++ database/migrations/2020_12_30_223844_create_companies_table.php
@@ -15,6 +15,12 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('domain')->unique();
+            $table->string('db_database')->unique();
+            $table->string('db_username');
+            $table->string('db_password');
+            $table->string('db_hostname');
             $table->timestamps();
         });
     }
